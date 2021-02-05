49ff92066b53f7b3a23b40412d10c4c7400601bb
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2021-02-05 17:21:59 +0200

    Complete exercise 1-12

---
+++ ch1/exercise_1-12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main()
+{
+  int c, state;
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' ||c == '\t') {
+      state = OUT;
+      // putchar("\\");
+      // putchar("n");
+      printf("\n");
+    } else {
+      state = IN;
+      putchar(c);
+    }
+  }
+
+  return 0;
+}
